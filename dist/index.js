31ad02bec4410c75b0011ac8a7f46aaa38ee7ba1
Matthew Pawley <matthew.james.pawley@gmail.com>
2018-02-01 14:52:12 +0000

    Compiles to ES5

---
+++ dist/index.js
@@ -0,0 +1,1 @@
+Object.defineProperty(exports,"__esModule",{value:true});var _api=require('./api');var _api2=_interopRequireDefault(_api);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj};}exports.default=new _api2.default();
---
+++ dist/utils.js
@@ -0,0 +1,1 @@
+Object.defineProperty(exports,"__esModule",{value:true});exports.default={excludeKeys:excludeKeys,isObject:isObject};function excludeKeys(obj,keys){keys=Array.isArray(keys)?keys:keys?Object.keys(keys):[];return Object.keys(obj).reduce(function(res,key){if(keys.indexOf(key)===-1){res[key]=obj[key];}return res;},{});}function isObject(obj){return typeof obj==='object'&&obj!==null;}
